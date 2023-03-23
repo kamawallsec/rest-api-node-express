3f86e0f3db3b11b140ae095e2630268fe7a3063f
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-23 12:39:45 +0100

    get request to the homepage

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res)=> {
+
+    console.log('Testing');
+    res.send('Homepage');
+
+})
+
 app.listen(PORT, ()=> console.log(`Server listening on http://localhost:${PORT}`));

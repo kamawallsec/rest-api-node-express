2ead55932d1f0e8fb087cf393119eed9dbc0a05f
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-23 15:28:01 +0100

    users  - console

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,24 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: "24"
+    },
+
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: "23"
+    }
+
+]
+
 router.get('/', (req, res)=> {  
+    console.log(users);
 
     res.send('Welcome user!');
 

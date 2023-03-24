4e900e4a48c89d961a4cad99509f6e749deda95d
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-24 19:30:04 +0100

    calling the functions for routing

--- routes/users.js
+++ routes/users.js
@@ -1,91 +1,16 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { createUser, getUsers, getSingleUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [
+router.get('/', getUsers);
 
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: "24"
-    },
+router.post('/', createUser);
 
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: "23"
-    }
+router.get('/:id', getSingleUser);
 
-]
+router.delete('/:id', deleteUser);
 
-router.get('/', (req, res)=> {  
-    console.log(users);
-
-    res.send('Welcome user!');
-
-});
-
-
-router.post('/', (req, res)=> {
-
-    // console.log('Post route ready');
-
-    const user = req.body;
-
-    const userId = uuidv4();   
-    const userWithId = { ...user, id: userId };
-    users.push(userWithId);
-
-    res.send(`User named ${user.firstName} has been added to the list of users`);
-
-});
-
-router.get('/:id', (req, res)=> {
-
-    const {id} = req.params;
-
-    const findUser = users.find(user => user.id === id);
-
-
-    // res.send('Id route!');
-    res.send(findUser);
-
-} );
-
-
-router.delete('/:id', (req, res)=> {
-
-    const {id} = req.params;
-
-    const deleteUser = users.filter((user) => user.id !== id);
-
-    res.send(`User ${deleteUser.firstName} ${id} has been removed from the list`)
-
-});
-
-
-router.patch('/:id', (req, res)=> {
-
-    const {id} = req.params;
-    const {firstName, lastName, age} = req.body;  // what can i change | frontend
-
-    const userUpdate = users.find((user)=> user.id === id );
-
-    if (firstName) {
-        userUpdate.firstName;
-    }
-
-    if (lastName) {
-        userUpdate.lastName;
-    }
-
-    if (age) {
-        userUpdate.age;
-    }
-
-    res.send(`User with the id ${id} has been updated 🥳`);
-
-});
+router.patch('/:id', updateUser);
 
 export default router;
